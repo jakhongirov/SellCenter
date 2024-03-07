504de0fc6cb6ce4ad205c99a7a1f7ab654c326d1
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-03-07 17:39:16 +0500

    add

--- src/modules/forklifts/forklifts.js
+++ src/modules/forklifts/forklifts.js
@@ -628,6 +628,11 @@ module.exports = {
                   status: 200,
                   message: "Success"
                })
+            } else {
+               return res.json({
+                  status: 400,
+                  message: "Bad request"
+               })
             }
 
          } else {
